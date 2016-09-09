a7f005db44bf3f1fb9a019d98e9fd1ee0e6d5e8e
Kyungtae Lee <ktlee@bitnine.net>
2016-09-09 15:39:41 +0900

    test: ODBC escape sequences

--- test/test_odbc.c
+++ test/test_odbc.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <sql.h>
 #include <sqlext.h>
 #include <stdlib.h>
@@ -92,7 +93,7 @@ void Test_MATCH(CuTest *tc)
 
 	SQLFreeStmt(hstmt, SQL_CLOSE);
 	
-	rc = SQLExecDirect(hstmt, (SQLCHAR*)"MATCH (n:person '{\"from\": \"Sweden\"}') RETURN n", SQL_NTS);
+	rc = SQLExecDirect(hstmt, (SQLCHAR*)"MATCH (n:person {'from': 'Sweden'}) RETURN n", SQL_NTS);
 	check_error("MATCH");
 
 	rc = AG_SQLDescribeCol(hstmt, 1, &agType, &agRawDataSize);
@@ -201,3 +202,215 @@ void Test_Bind_WHERE(CuTest *tc)
 	disconnectDB();
 }
 
+void Test_Bind_MATCH_Property(CuTest *tc)
+{
+	SQLINTEGER two;
+	SQLLEN ind = SQL_NTS;
+	SQLCHAR from[10] = "Emil";
+	SQLCHAR *buffer;
+	SQLSMALLINT agType;
+	SQLLEN agRawDataSize;
+	struct ag_vertex *v;
+
+	rc = connectDB();
+	CuAssertTrue(tc, SQL_SUCCEEDED(rc));
+
+	rc = SQLPrepare(hstmt, (SQLCHAR*)"MATCH ( n:person { 'name' : ?::text } )  RETURN n", SQL_NTS);
+	check_error("SQLPrepare");
+
+	rc = SQLBindParameter(hstmt, 1, SQL_PARAM_INPUT, SQL_C_CHAR, SQL_CHAR, 10, 0, from, 0, &ind);
+	check_error("SQLBindParameter");
+
+	rc = SQLExecute(hstmt);
+	check_error("SQLExecute");
+
+	rc = AG_SQLDescribeCol(hstmt, 1, &agType, &agRawDataSize);
+	check_error("AG_SQLDescribeCol");
+
+	rc = SQLFetch(hstmt);
+	check_error("SQLFetch");
+
+	buffer = (SQLCHAR *)malloc(agRawDataSize);
+	rc = AG_SQLGetData(hstmt, 1, agType, (void **)&v, buffer, agRawDataSize, &ind);
+	check_error("AG_SQLGetData");
+
+	CuAssertIntEquals(tc, 99, ag_json_get_int(ag_json_object_get(v->props, "klout")));
+
+	free(buffer);
+	ag_vertex_free(v);
+
+	SQLFreeStmt(hstmt, SQL_CLOSE);
+
+	disconnectDB();
+}
+
+void Test_Bind_ODBC_escape(CuTest *tc)
+{
+	SQLLEN ind = SQL_NTS;
+	SQLCHAR *buffer;
+	SQLSMALLINT agType;
+	SQLLEN agRawDataSize;
+	ag_json age;
+
+	rc = connectDB();
+	CuAssertTrue(tc, SQL_SUCCEEDED(rc));
+
+	rc = SQLExecDirect(hstmt, (SQLCHAR*)"CREATE (n:person {'name': 'Emil', 'from': 'KOREA', 'age': 41})", SQL_NTS);
+	check_error("CREATE");
+
+	SQLFreeStmt(hstmt, SQL_CLOSE);
+	
+	rc = SQLExecDirect(hstmt, 
+		(SQLCHAR*)"MATCH ( n:person { 'from' : {fn UCASE('Korea')} } ) "
+		"RETURN (n).age", SQL_NTS);
+	check_error("SQLExecDirect");
+
+	rc = SQLFetch(hstmt);
+	check_error("SQLFetch");
+
+	rc = AG_SQLDescribeCol(hstmt, 1, &agType, &agRawDataSize);
+	check_error("AG_SQLDescribeCol");
+	
+	buffer = (SQLCHAR *)malloc(agRawDataSize);
+	rc = AG_SQLGetData(hstmt, 1, agType, (void **)&age, buffer, agRawDataSize, &ind);
+	check_error("AG_SQLGetData");
+
+	CuAssertIntEquals(tc, 41, ag_json_get_int(age));
+
+	SQLFreeStmt(hstmt, SQL_CLOSE);
+
+	disconnectDB();
+}
+
+
+/* from "psqlodbc-09.05.0400/test/src/" */
+
+#define bindParamString(hstmt, paramno, str) \
+	_bindParamString(hstmt, paramno, str, tc)
+
+#define bindOutParamString(hstmt, paramno, outbuf, outbuflen) \
+	_bindOutParamString(hstmt, paramno, outbuf, outbuflen, tc)
+
+#define executeQuery(hstmt) \
+	_executeQuery(hstmt, tc)
+
+#define prepareQuery(hstmt, str) \
+	_prepareQuery(hstmt, str, tc)
+
+static void
+_bindParamString(HSTMT hstmt, int paramno, char *str, CuTest *tc)
+{
+	SQLRETURN	rc;
+	static SQLLEN		cbParams[10];
+
+	cbParams[paramno] = SQL_NTS;
+	rc = SQLBindParameter(hstmt, paramno, SQL_PARAM_INPUT,
+						  SQL_C_CHAR,	/* value type */
+						  SQL_CHAR,		/* param type */
+						  20,			/* column size */
+						  0,			/* dec digits */
+						  str,		/* param value ptr */
+						  0,			/* buffer len */
+						  &cbParams[paramno]		/* StrLen_or_IndPtr */);
+	check_error("SQLBindParameter failed");
+}
+
+static void
+_bindOutParamString(HSTMT hstmt, int paramno, char *outbuf, int outbuflen, CuTest *tc)
+{
+	static SQLLEN cbParams[10];
+
+    rc = SQLBindParameter(hstmt, paramno, SQL_PARAM_OUTPUT,
+						  SQL_C_CHAR,	/* value type */
+						  SQL_CHAR,		/* param type */
+						  20,			/* column size */
+						  0,			/* dec digits */
+						  outbuf,		/* param value ptr */
+						  outbuflen,	/* buffer len */
+						  &cbParams[paramno]		/* StrLen_or_IndPtr */);
+	check_error("SQLBindParameter failed");
+}
+
+static void
+_executeQuery(HSTMT hstmt, CuTest *tc)
+{
+	SQLRETURN	rc;
+
+	rc = SQLExecute(hstmt);
+	check_error("SQLExecute failed");
+	rc = SQLFreeStmt(hstmt, SQL_CLOSE);
+	check_error("SQLFreeStmt failed");
+}
+
+static void
+_prepareQuery(HSTMT hstmt, char *str, CuTest *tc)
+{
+	SQLRETURN	rc;
+
+	rc = SQLPrepare(hstmt, (SQLCHAR *) str, SQL_NTS);
+	check_error("SQLPrepare failed");
+}
+
+void Test_odbc_escapes(CuTest *tc)
+{
+	char outbuf[10];
+	rc = connectDB();
+	CuAssertTrue(tc, SQL_SUCCEEDED(rc));
+
+	/**** Function escapes ****/
+
+	/* CONCAT */
+	prepareQuery(hstmt, "SELECT {fn CONCAT(?, ?) }");
+	bindParamString(hstmt, 1, "foo");
+	bindParamString(hstmt, 2, "bar");
+	executeQuery(hstmt);
+
+	/* LOCATE */
+	prepareQuery(hstmt, "SELECT {fn LOCATE(?, ?, 2) }");
+	bindParamString(hstmt, 1, "needle");
+	bindParamString(hstmt, 2, "this is a needle in an ol' haystack");
+	executeQuery(hstmt);
+
+	/* SPACE */
+	prepareQuery(hstmt, "SELECT 'x' || {fn SPACE(10) } || 'x'");
+	executeQuery(hstmt);
+
+	/**** CALL escapes ****/
+
+	prepareQuery(hstmt, "{ call length(?) }");
+	bindParamString(hstmt, 1, "foobar");
+	executeQuery(hstmt);
+
+	prepareQuery(hstmt, "{ call right(?, ?) }");
+	bindParamString(hstmt, 1, "foobar");
+	bindParamString(hstmt, 2, "3");
+	executeQuery(hstmt);
+
+	prepareQuery(hstmt, "{ ? = call length('foo') }");
+	memset(outbuf, 0, sizeof(outbuf));
+	bindOutParamString(hstmt, 1, outbuf, sizeof(outbuf) - 1);
+	executeQuery(hstmt);
+
+	/* TODO: This doesn't currently work.
+	prepareQuery(hstmt, "{ ? = call concat(?, ?) }");
+	memset(outbuf, 0, sizeof(outbuf));
+	bindOutParamString(hstmt, 1, outbuf, sizeof(outbuf) - 1);
+	bindParamString(hstmt, 2, "foo");
+	bindParamString(hstmt, 3, "bar");
+	executeQuery(hstmt);
+	printf("OUT param: %s\n", outbuf);
+	*/
+
+	/**** Date, Time, and Timestamp literals ****/
+
+	prepareQuery(hstmt, "SELECT {d '2014-12-21' } + '1 day'::interval");
+	executeQuery(hstmt);
+
+	prepareQuery(hstmt, "SELECT {t '20:30:40' } + '1 hour 1 minute 1 second'::interval");
+	executeQuery(hstmt);
+
+	prepareQuery(hstmt, "SELECT {ts '2014-12-21 20:30:40' } + '1 day 1 hour 1 minute 1 second'::interval");
+	executeQuery(hstmt);
+
+	disconnectDB();
+}
